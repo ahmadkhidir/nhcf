940e32a971746574fdac4ecbf6de6642fef114df
Ahmad Khidir <khidirahmad05@gmail.com>
2022-09-21 06:50:18 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App(props) {
     <main className='App'>
       <picture>
         <source srcSet={mobile} media='(max-width: 500px)' draggable={false}></source>
-        <img srcSet={web} draggable={false} />
+        <img srcSet={web} draggable={false} alt='' />
       </picture>
       <section className='button'>
         <p>This event requires you to pre-register</p>
-        <a href="https://forms.office.com/r/bmatb9hsVj">Start Registration</a>
+        <a href="https://forms.office.com/r/bmatb9hsVj" >Start Registration</a>
       </section>
     </main>
   )
